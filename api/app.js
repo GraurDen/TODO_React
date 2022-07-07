a63bba54e168582867b16eec7d9e24494d205d74
GraurDen <graurden@gmail.com>
2022-07-07 21:50:01 +0300

    hide logging in terminal

--- api/app.js
+++ api/app.js
@@ -1,13 +1,13 @@
-require('dotenv').config();
-const recursive = require('recursive-readdir-sync');
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const recursive = require("recursive-readdir-sync");
+const express = require("express");
+const cors = require("cors");
 const port = process.env.PORT;
 const app = express();
-const sequelize = require('./database');
-const i18next = require('i18next');
-const Backend = require('i18next-fs-backend');
-const i18nextMiddleware = require('i18next-http-middleware');
+const sequelize = require("./database");
+const i18next = require("i18next");
+const Backend = require("i18next-fs-backend");
+const i18nextMiddleware = require("i18next-http-middleware");
 
 i18next
     .use(Backend)
@@ -15,16 +15,16 @@ i18next
     .init({
         //Path to language resource files
         backend: {
-            loadPath: __dirname + '/resources/locales/{{lng}}/{{ns}}.json',
+            loadPath: __dirname + "/resources/locales/{{lng}}/{{ns}}.json",
         },
         // where to look language
         detection: {
             // detect lang, check query string first
-            order: ['querystring', 'cookie'],
-            caches: ['cookie'],
+            order: ["querystring", "cookie"],
+            caches: ["cookie"],
         },
-        fallbackLng: 'en', // set 'en' if no active parameters found anywhere
-        preload: ['en', 'ru'],
+        fallbackLng: "en", // set 'en' if no active parameters found anywhere
+        preload: ["en", "ru"],
     });
 
 app.use(i18nextMiddleware.handle(i18next));
@@ -33,16 +33,17 @@ app.use(express.json());
 app.use(cors());
 
 recursive(`${__dirname}/routes`).forEach((file) =>
-    app.use('/api', require(file))
+    app.use("/api", require(file))
 );
 
 async function startApp() {
     try {
         await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-        app.listen(port, () => console.log('SERVER STARTED ON PORT: ' + port));
+
+        console.log("Connection has been established successfully.");
+        app.listen(port, () => console.log("SERVER STARTED ON PORT: " + port));
     } catch (e) {
-        console.e('Unable to connect to the database:', e);
+        console.e("Unable to connect to the database:", e);
         console.log(e);
     }
 }
--- api/config/config.js
+++ api/config/config.js
@@ -1,11 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
     development: {
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASS,
-        database: 'todo_db',
+        database: "todo_db",
         host: process.env.HOST,
-        dialect: 'postgres',
+        dialect: "postgres",
+        logging: false,
     },
 };
